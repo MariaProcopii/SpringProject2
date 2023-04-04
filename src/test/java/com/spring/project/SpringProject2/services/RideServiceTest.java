a921b2e0f55ab881a69879f5e48d8aec20d409b9
MariaProcopii <mari.procopii@gmail.com>
2023-04-05 01:52:49 +0300

    made testFindOne() for RideServiceTest

--- src/test/java/com/spring/project/SpringProject2/services/RideServiceTest.java
+++ src/test/java/com/spring/project/SpringProject2/services/RideServiceTest.java
@@ -4,6 +4,9 @@ import com.spring.project.SpringProject2.repositories.RideRepository;
 import org.junit.jupiter.api.*;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 class RideServiceTest {
@@ -24,6 +27,16 @@ class RideServiceTest {
         Ride ride = new Ride();
         rideService.save(ride);
         verify(rideRepository, times(1)).save(ride);
+    }
+
+    @Test
+    void testFindOne() {
+        int id = 1;
+        Ride ride = new Ride();
+        ride.setId(id);
+        when(rideRepository.findById(id)).thenReturn(Optional.of(ride));
+        Ride foundRide = rideService.findOne(id);
+        assertEquals(foundRide, ride);
     }
 }
 
