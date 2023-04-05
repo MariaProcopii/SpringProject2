6450e0e2738d6c301001538c35e3c9a1837fa0a4
MariaProcopii <mari.procopii@gmail.com>
2023-04-05 18:34:38 +0300

    deleted the AuthProvider. Automatically set the authorization process.

--- src/main/java/com/spring/project/SpringProject2/config/SecurityConfig.java
+++ src/main/java/com/spring/project/SpringProject2/config/SecurityConfig.java
@@ -1,22 +1,32 @@
 package com.spring.project.SpringProject2.config;
 
-import com.spring.project.SpringProject2.security.AuthProvider;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.spring.project.SpringProject2.services.PeopleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthProvider authProvider;
+    private final PeopleService peopleService;
 
     @Autowired
-    public SecurityConfig(AuthProvider authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PeopleService peopleService) {
+        this.peopleService = peopleService;
     }
 
-    protected void configure(AuthenticationManagerBuilder auth){
-        auth.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(peopleService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
